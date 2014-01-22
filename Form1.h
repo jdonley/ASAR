6c939c1e0f32ce744460e2e09be51054460a4f6d
Jacob <JacobD_10@hotmail.com>
2014-01-22 20:30:33 +1100

    Added some initial ideas

--- Form1.h
+++ Form1.h
@@ -34,6 +34,12 @@ namespace ASAR {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected: 
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
 
 	private:
 		/// <summary>
@@ -48,13 +54,89 @@ namespace ASAR {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(90, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(119, 51);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Rotate Left";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(215, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(121, 51);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Rotate Right";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(86, 82);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(258, 24);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Current Position: 180 degrees";
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->button3->Location = System::Drawing::Point(12, 163);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(103, 52);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Start Recording";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->button4->Location = System::Drawing::Point(190, 141);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(261, 74);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"Start 360 degree Recording";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(463, 227);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form1";
+			this->ShowIcon = false;
+			this->Text = L"Automated Spatial Audio Recorder";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+			 }
+};
 }
 
