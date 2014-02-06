1e4bcbbd0003a4c5a60593a66bcf2b2718afe83c
Jacob <JacobD_10@hotmail.com>
2014-02-07 00:20:23 +1100

    Added some UI Automation to the C++ project

--- ASAR_cpp/ASAR.cpp
+++ ASAR_cpp/ASAR.cpp
@@ -9,11 +9,11 @@ using namespace ASAR;
 int main(array<System::String ^> ^args)
 {
 	// Enabling Windows XP visual effects before any controls are created
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false); 
+	System::Windows::Forms::Application::EnableVisualStyles();
+	System::Windows::Forms::Application::SetCompatibleTextRenderingDefault(false);
 
 	// Create the main window and run it
-	Application::Run(gcnew Form1());
+	System::Windows::Forms::Application::Run(gcnew Form1());
 	
 	return 0;
 }
--- ASAR_cpp/Form1.h
+++ ASAR_cpp/Form1.h
@@ -3,11 +3,17 @@
 namespace ASAR {
 
 	using namespace System;
+	using namespace System::IO;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+
+	//TestStack.White (UI Automation)
+	using namespace TestStack::White;
+	using namespace TestStack::White::WindowsAPI;
+	using namespace TestStack::White::UIItems;
 
 	/// <summary>
 	/// Summary for Form1
@@ -15,6 +21,17 @@ namespace ASAR {
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
 		DataVisualization::Charting::Series ^ seriesLocation;
+
+		TestStack::White::Application ^ appAudacity;
+		TestStack::White::UIItems::WindowItems::Window ^ audacityWindow;
+		TestStack::White::UIItems::Panel ^ audacityToolDock;
+		TestStack::White::UIItems::Panel ^ audacityTransportToolbar;
+		TestStack::White::UIItems::Button ^ AudacityButton_Record;
+		TestStack::White::UIItems::Button ^ AudacityButton_Pause;
+		TestStack::White::UIItems::Button ^ AudacityButton_Play;
+		TestStack::White::UIItems::Button ^ AudacityButton_Stop;
+		TestStack::White::UIItems::Button ^ AudacityButton_SkipToStart;
+		TestStack::White::UIItems::Button ^ AudacityButton_SkipToEnd;
 
 	public:
 		Form1(void)
@@ -54,6 +71,14 @@ namespace ASAR {
 
 	private: System::Windows::Forms::Label^  lblCurrentPosition;
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chartLocation;
+	private: System::Windows::Forms::Button^  btnAudacitySkipToStart;
+	private: System::Windows::Forms::Button^  btnAudacityPause;
+	private: System::Windows::Forms::Button^  btnAudacityStop;
+	private: System::Windows::Forms::Button^  btnAudacitySkipToEnd;
+	private: System::Windows::Forms::Button^  btnAudacityPlay;
+	private: System::Windows::Forms::Button^  btnAudacityRecord;
+	private: System::Windows::Forms::Button^  btnAudacity;
+	private: System::Windows::Forms::Label^  label3;
 
 
 	private: System::ComponentModel::IContainer^  components;
@@ -72,10 +97,10 @@ namespace ASAR {
 		void InitializeComponent(void)
 		{
 			this->components = (gcnew System::ComponentModel::Container());
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^  series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
-			System::Windows::Forms::DataVisualization::Charting::Title^  title2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Title());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::Title^  title1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Title());
 			this->btnRotateCCW = (gcnew System::Windows::Forms::Button());
 			this->btnRotateCW = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
@@ -86,6 +111,14 @@ namespace ASAR {
 			this->serialPort = (gcnew System::IO::Ports::SerialPort(this->components));
 			this->lblCurrentPosition = (gcnew System::Windows::Forms::Label());
 			this->chartLocation = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->btnAudacitySkipToStart = (gcnew System::Windows::Forms::Button());
+			this->btnAudacityPause = (gcnew System::Windows::Forms::Button());
+			this->btnAudacityStop = (gcnew System::Windows::Forms::Button());
+			this->btnAudacitySkipToEnd = (gcnew System::Windows::Forms::Button());
+			this->btnAudacityPlay = (gcnew System::Windows::Forms::Button());
+			this->btnAudacityRecord = (gcnew System::Windows::Forms::Button());
+			this->btnAudacity = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chartLocation))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -177,38 +210,135 @@ namespace ASAR {
 			// chartLocation
 			// 
 			this->chartLocation->BackColor = System::Drawing::Color::Transparent;
-			chartArea2->Name = L"ChartArea1";
-			this->chartLocation->ChartAreas->Add(chartArea2);
-			legend2->Enabled = false;
-			legend2->Name = L"Legend1";
-			this->chartLocation->Legends->Add(legend2);
+			chartArea1->Name = L"ChartArea1";
+			this->chartLocation->ChartAreas->Add(chartArea1);
+			legend1->Enabled = false;
+			legend1->Name = L"Legend1";
+			this->chartLocation->Legends->Add(legend1);
 			this->chartLocation->Location = System::Drawing::Point(15, 206);
 			this->chartLocation->Name = L"chartLocation";
-			series2->BorderWidth = 5;
-			series2->ChartArea = L"ChartArea1";
-			series2->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Polar;
-			series2->Legend = L"Legend1";
-			series2->MarkerBorderColor = System::Drawing::Color::Blue;
-			series2->MarkerBorderWidth = 2;
-			series2->MarkerSize = 15;
-			series2->MarkerStep = 2;
-			series2->MarkerStyle = System::Windows::Forms::DataVisualization::Charting::MarkerStyle::Circle;
-			series2->Name = L"Location";
-			series2->YAxisType = System::Windows::Forms::DataVisualization::Charting::AxisType::Secondary;
-			this->chartLocation->Series->Add(series2);
+			series1->BorderWidth = 5;
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Polar;
+			series1->Legend = L"Legend1";
+			series1->MarkerBorderColor = System::Drawing::Color::Blue;
+			series1->MarkerBorderWidth = 2;
+			series1->MarkerSize = 15;
+			series1->MarkerStep = 2;
+			series1->MarkerStyle = System::Windows::Forms::DataVisualization::Charting::MarkerStyle::Circle;
+			series1->Name = L"Location";
+			series1->YAxisType = System::Windows::Forms::DataVisualization::Charting::AxisType::Secondary;
+			this->chartLocation->Series->Add(series1);
 			this->chartLocation->Size = System::Drawing::Size(391, 316);
 			this->chartLocation->TabIndex = 8;
-			title2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			title1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			title2->Name = L"Location";
-			title2->Text = L"Location";
-			this->chartLocation->Titles->Add(title2);
+			title1->Name = L"Location";
+			title1->Text = L"Location";
+			this->chartLocation->Titles->Add(title1);
+			// 
+			// btnAudacitySkipToStart
+			// 
+			this->btnAudacitySkipToStart->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->btnAudacitySkipToStart->Location = System::Drawing::Point(577, 147);
+			this->btnAudacitySkipToStart->Name = L"btnAudacitySkipToStart";
+			this->btnAudacitySkipToStart->Size = System::Drawing::Size(100, 50);
+			this->btnAudacitySkipToStart->TabIndex = 34;
+			this->btnAudacitySkipToStart->Text = L"Skip to Start";
+			this->btnAudacitySkipToStart->UseVisualStyleBackColor = true;
+			// 
+			// btnAudacityPause
+			// 
+			this->btnAudacityPause->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAudacityPause->Location = System::Drawing::Point(577, 91);
+			this->btnAudacityPause->Name = L"btnAudacityPause";
+			this->btnAudacityPause->Size = System::Drawing::Size(100, 50);
+			this->btnAudacityPause->TabIndex = 33;
+			this->btnAudacityPause->Text = L"Pause";
+			this->btnAudacityPause->UseVisualStyleBackColor = true;
+			// 
+			// btnAudacityStop
+			// 
+			this->btnAudacityStop->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAudacityStop->Location = System::Drawing::Point(789, 91);
+			this->btnAudacityStop->Name = L"btnAudacityStop";
+			this->btnAudacityStop->Size = System::Drawing::Size(100, 50);
+			this->btnAudacityStop->TabIndex = 32;
+			this->btnAudacityStop->Text = L"Stop";
+			this->btnAudacityStop->UseVisualStyleBackColor = true;
+			// 
+			// btnAudacitySkipToEnd
+			// 
+			this->btnAudacitySkipToEnd->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAudacitySkipToEnd->Location = System::Drawing::Point(683, 147);
+			this->btnAudacitySkipToEnd->Name = L"btnAudacitySkipToEnd";
+			this->btnAudacitySkipToEnd->Size = System::Drawing::Size(100, 50);
+			this->btnAudacitySkipToEnd->TabIndex = 31;
+			this->btnAudacitySkipToEnd->Text = L"Skip to End";
+			this->btnAudacitySkipToEnd->UseVisualStyleBackColor = true;
+			// 
+			// btnAudacityPlay
+			// 
+			this->btnAudacityPlay->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAudacityPlay->Location = System::Drawing::Point(683, 91);
+			this->btnAudacityPlay->Name = L"btnAudacityPlay";
+			this->btnAudacityPlay->Size = System::Drawing::Size(100, 50);
+			this->btnAudacityPlay->TabIndex = 30;
+			this->btnAudacityPlay->Text = L"Play";
+			this->btnAudacityPlay->UseVisualStyleBackColor = true;
+			// 
+			// btnAudacityRecord
+			// 
+			this->btnAudacityRecord->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAudacityRecord->Location = System::Drawing::Point(789, 147);
+			this->btnAudacityRecord->Name = L"btnAudacityRecord";
+			this->btnAudacityRecord->Size = System::Drawing::Size(100, 50);
+			this->btnAudacityRecord->TabIndex = 29;
+			this->btnAudacityRecord->Text = L"Record";
+			this->btnAudacityRecord->UseVisualStyleBackColor = true;
+			// 
+			// btnAudacity
+			// 
+			this->btnAudacity->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnAudacity->Location = System::Drawing::Point(577, 35);
+			this->btnAudacity->Name = L"btnAudacity";
+			this->btnAudacity->Size = System::Drawing::Size(312, 50);
+			this->btnAudacity->TabIndex = 28;
+			this->btnAudacity->Text = L"Start Audacity";
+			this->btnAudacity->UseVisualStyleBackColor = true;
+			this->btnAudacity->Click += gcnew System::EventHandler(this, &Form1::btnAudacity_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(574, 15);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(96, 17);
+			this->label3->TabIndex = 27;
+			this->label3->Text = L"UI Automation";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(583, 534);
+			this->ClientSize = System::Drawing::Size(994, 534);
+			this->Controls->Add(this->btnAudacitySkipToStart);
+			this->Controls->Add(this->btnAudacityPause);
+			this->Controls->Add(this->btnAudacityStop);
+			this->Controls->Add(this->btnAudacitySkipToEnd);
+			this->Controls->Add(this->btnAudacityPlay);
+			this->Controls->Add(this->btnAudacityRecord);
+			this->Controls->Add(this->btnAudacity);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->chartLocation);
 			this->Controls->Add(this->lblCurrentPosition);
 			this->Controls->Add(this->textBox1);
@@ -233,7 +363,7 @@ namespace ASAR {
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
+
 		/* Attn. Matthew Dabin
 		* Feel free to start writing code here which will connect
 		* to the COM port of choice and you can display the output
@@ -281,6 +411,26 @@ namespace ASAR {
 				 seriesLocation->Points[0]->XValue += step;
 	}
 
+private: System::Void btnAudacity_Click(System::Object^  sender, System::EventArgs^  e) {
+
+			 if (!File::Exists(Environment::GetFolderPath(Environment::SpecialFolder::ProgramFiles) + "\\Audacity\\Audacity.exe"))
+			 {
+				 MessageBox::Show("Audacity is not installed...");
+				 return;
+			 }
+			 appAudacity = TestStack::White::Application::Launch(Environment::GetFolderPath(Environment::SpecialFolder::ProgramFiles) + "\\Audacity\\Audacity.exe");
+
+			 audacityWindow = appAudacity->GetWindow("Audacity", TestStack::White::Factory::InitializeOption::NoCache);
+			 audacityToolDock = (TestStack::White::UIItems::Panel ^)audacityWindow->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("ToolDock"))[0];
+			 audacityTransportToolbar = (TestStack::White::UIItems::Panel ^)audacityToolDock->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Audacity Transport ToolBar"))[0];
+
+			 AudacityButton_Record = (TestStack::White::UIItems::Button ^)audacityTransportToolbar->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Record"))[0];
+			 AudacityButton_Pause = (TestStack::White::UIItems::Button ^)audacityTransportToolbar->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Pause"))[0];
+			 AudacityButton_Play = (TestStack::White::UIItems::Button ^)audacityTransportToolbar->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Play"))[0];
+			 AudacityButton_Stop = (TestStack::White::UIItems::Button ^)audacityTransportToolbar->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Stop"))[0];
+			 AudacityButton_SkipToStart = (TestStack::White::UIItems::Button ^)audacityTransportToolbar->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Skip to Start"))[0];
+			 AudacityButton_SkipToEnd = (TestStack::White::UIItems::Button ^)audacityTransportToolbar->GetMultiple(TestStack::White::UIItems::Finders::SearchCriteria::ByText("Skip to End"))[0];
+}
 };
 }
 
