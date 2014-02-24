578d8de1b157d6016776d7c04d28cfb77080804c
Jacob <JacobD_10@hotmail.com>
2014-02-24 23:55:01 +1100

    Removed Matt's project and C++ version. Continuing with C# and Matt's project was merged to master.

--- ASAR/Form1.Designer.cs
+++ ASAR/Form1.Designer.cs
@@ -528,7 +528,7 @@
             this.tabFrequency.Location = new System.Drawing.Point(4, 29);
             this.tabFrequency.Name = "tabFrequency";
             this.tabFrequency.Padding = new System.Windows.Forms.Padding(3);
-            this.tabFrequency.Size = new System.Drawing.Size(278, 368);
+            this.tabFrequency.Size = new System.Drawing.Size(279, 368);
             this.tabFrequency.TabIndex = 1;
             this.tabFrequency.Text = "Frequency";
             this.tabFrequency.UseVisualStyleBackColor = true;
