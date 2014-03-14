1098a2b41c75d23394a30f40210a13b8744c3c69
Jacob <JacobD_10@hotmail.com>
2014-03-14 17:22:55 +1100

    Added Web Access Tutorial

---
+++ ASAR/Controllers/ProgressController.cs
@@ -0,0 +1,39 @@
+﻿namespace ASAR
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Http;
+
+    public class ProgressController : ApiController
+    {
+        Progress[] products = new Progress[]  
+        {  
+            new Progress { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },  
+            new Progress { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },  
+            new Progress { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }  
+        };
+
+        public IEnumerable<Progress> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Progress GetProductById(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return product;
+        }
+
+        public IEnumerable<Progress> GetProductsByCategory(string category)
+        {
+            return products.Where(p => string.Equals(p.Category, category,
+                    StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ ASAR/Progress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASAR
+{
+    public class Progress
+    {
+        //public string   Status        { get; set; }
+        //public string   RecordingType { get; set; }
+        //public int      Position      { get; set; }
+        //public DateTime LapsedTime    { get; set; }
+        //public TimeSpan RemainingTime { get; set; }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
